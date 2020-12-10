0dc7b9faef04078f55004bb2e800a475b6d84e20
badminininja <josephnm911@gmail.com>
2020-12-10 02:28:44 -0800

    implemented beach Scene One

--- CrashedAndDeserted/CrashedAndDeserted/main.cpp
+++ CrashedAndDeserted/CrashedAndDeserted/main.cpp
@@ -9,18 +9,20 @@
 #include "characterRace.hpp"
 #include "animalRace.hpp"
 #include "humanRace.hpp"
-#include "ExiledMember.hpp"
-#include "Boss.hpp"
-#include "Goblin.hpp"
+//#include "ExiledMember.hpp"
+//#include "Boss.hpp"
+//#include "Goblin.hpp"
 #include "Dice20App.hpp"
 
 using namespace std;
 
 Character* openingScene(CharacterRace* typeCharacter, Character* userCharacter);
 int firstScene(Character* userCharacter, vector<string> progress);
-bool GoblinBattle(Character* player);
-bool ExiledMemberBattle(Character* player);
-bool BossBattle(Character* player);
+int beachScene(Character* userCharacter, vector<string> progress);
+int BeachDialogue();
+//bool GoblinBattle(Character* player);
+//bool ExiledMemberBattle(Character* player);
+//bool BossBattle(Character* player);
 
 int main() {
     CharacterRace* typeCharacter;
@@ -30,6 +32,7 @@ int main() {
     //Will have to make a Character class that inherits the Mage and Warrior classes
     userCharacter = openingScene(typeCharacter,userCharacter);
     sceneNum = firstScene(userCharacter, progress);
+    int tempBeachScene = beachScene(userCharacter, progress);
     return 0;
 }
 
@@ -116,7 +119,85 @@ int firstScene(Character* userCharacter, vector<string> progress) {
     }
     return scene;
 }
+int beachScene(Character* userCharacter, vector<string> progress){
+	int response = 0;
+	cout<< "As you go down the beach, you notice how clear it is. Just sand and rocks. You don’t spot any pollution around or littered on the beach; until you come across what seems to be some near a rock cluster. You move to the side to get a better view of what might be behind the rock and you see someone who is sleeping there, understandably so as the sun has just risen."<<endl;
+	cout << "What do you do?"<<endl;
+	cout << "Call out to him from afar to wake him up and talk to him? (Enter 1)"<<endl;
+	cout << "Go up to him and shake him to make sure he wakes up? You gotta be sure he wakes up else you will continue to be clueless of this situation. (Enter 2)"<<endl;
+	cout << "Ignore him and keep walking along the beach? Who knows who or what he is, it's best to just leave him be. Plus, he is sleeping so might as well let him sleep. (Enter 3)" <<endl;
+	while((response !=1) && (response !=2) && (response !=3)){
+		cin >> response;
+		if(response ==1){
+			cout << "The guy sleeping woke up and noticed you. He does an inviting gesture and you walk up a bit closer so you can talk to him." <<endl;//DIALOGUE
+			return BeachDialogue();
+		}
+		else if(response == 2){
+			cout << "As you shake him vigorously to wake him up, the guy gets startled and wakes up; however, as a defensive reaction to his sudden awakening he hit you with a rock that he was holding while sleeping. (Player takes damage) “Oh man, sorry, I didn’t mean to hit you, it's just that I thought you might have been like a bear or something, it was just a reflex” the stranger says. He then sits up and asks if you want to know anything as it was the least he could do." <<endl; //DIALOGUE
+			return BeachDialogue();	
+		}
+		else if (response == 3){
+			cout << "As you walk past him you start to notice how run down his area was, but it’s none of your business. As you walk a few paces past what you could only assume was his area, you start to hear a waterfall nearby and that's when you get stabbed in the back from the guy that was supposedly sleeping. (Player takes damage). The guy was never actually sleeping and you simply showed your back to a potential enemy. You were an exiled on this beach, if he’s there then he must be one too. He loads up a second strike but he you parry it and you guys fight." <<endl;
+			//call Battle Function
+		}
+		else{
+			cout << "Call out to him from afar to wake him up and talk to him? (Enter 1)"<<endl;
+        		cout << "Go up to him and shake him to make sure he wakes up? You gotta be sure he wakes up else you will continue to be clueless of this situation. (Enter 2)"<<endl;
+        		cout << "Ignore him and keep walking along the beach? Who knows who or what he is, it's best to just leave him be. Plus, he is sleeping so might as well let him sleep. (Enter 3)" <<endl;
+		}
+	}
 
+
+}
+int BeachDialogue(){
+	int response = 0;
+	cout << "Hey, my name is Octavius, Tane Octavius, I’ve been here for a short while now, is there anything you wanted to know? I don’t have much better things to do right now and I don’t mind the company." <<endl;
+	cout << "What do you want to know?" <<endl;
+	cout << "Ask about the area (Enter 1)" <<endl;
+	cout << "Ask how he got here (Enter 2)" <<endl;
+	cout << "Ask if he knows if there is anyone else around (Enter 3)" <<endl;
+	cout << "Ask why is he here on this island (Enter 4)" << endl;
+	cout << "You know all that you need to know, say a farewell and continue along the beach (Enter 5)" << endl;
+	while((response !=4) && (response !=5)){
+		cin >>response;
+		if (response == 1){
+			cout << "So far as I know, this place is a small island, located who knows where. The forest covers most of the area, there’s plenty of food in there so it's pretty easy to continue living here." <<endl;
+			cout << "Anything else you want to know? (1 area, 2 how, 3 anyone, 4 why, 5 leave)" <<endl;
+		}
+		else if (response == 2){
+			cout << "I’m not entirely sure how I got here, at one point I got knocked out and then I woke up on this Island, I don’t know how I was put here nor the people who did it. Though that was a long time ago so that matters little now." <<endl;
+                        cout << "Anything else you want to know? (1 area, 2 how, 3 anyone, 4 why, 5 leave)" <<endl;
+		}	
+		else if (response == 3){
+			cout << "I haven’t actually seen anyone around but I do hear noises that I can only assume are other people. Or maybe even mystical creatures, who knows. I don’t know their business whether they are hostile or friendly so I haven’t confronted them." <<endl;
+                        cout << "Anything else you want to know? (1 area, 2 how, 3 anyone, 4 why, 5 leave)" <<endl;
+
+		}
+		else if (response == 4){
+			cout << "How about before I answer that we go into the forest? It's morning and I haven’t had anything to eat and I’m sure you haven’t either. There is a lot of food in the forest as it seems there has been little human intervention." <<endl;
+			//BATTLE TRIGGER
+			cout << "You both start walking into the forest with Tane leading to a place with some good fruit. “You know I remembered the reason why I’m here, before I was knocked out and landed on this beach, I remember a court scene. Heh, yea I was exiled. But you know, that also means you’ve been exiled, and that means you’ve got blood on your hands” As we finish his sentence an arrow shoots, hidden in some bushes and pierces your shoulder, just nearly missing your head. “Tsk, You’re a criminal like me, I can’t trust you” He pulls out his sword and gets ready to fight you." <<endl;
+			return response;
+		}
+		else if (response == 5){
+			cout << "All good, I’m gonna go get some fruit in the forest for something to eat, see ya around I guess" <<endl;
+			return response;
+		}
+		else{
+			cout << "What do you want to know?" <<endl;
+		        cout << "Ask about the area (Enter 1)" <<endl;
+		        cout << "Ask how he got here (Enter 2)" <<endl;
+		        cout << "Ask if he knows if there is anyone else around (Enter 3)" <<endl;
+		        cout << "Ask why is he here on this island (Enter 4)" << endl;
+		        cout << "You know all that you need to know, say a farewell and continue along the beach (Enter 5)" << endl;
+	
+		
+		}
+
+
+	}
+}
+/*
 bool GoblinBattle(Character* player) {
 	Goblin* goblinObj = new Goblin();
 	double bias = player->getIQ() - goblinObj->getHealth();
@@ -458,5 +539,5 @@ bool BossBattle(Character* player) {
         return true;
     }
 }
-
+*/
 
