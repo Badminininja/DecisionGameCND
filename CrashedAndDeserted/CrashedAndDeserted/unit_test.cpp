71a32ebb0de576de80bc440bf046ac2ae7c8a41f
aramismas <aramismasarati@yahoo.com>
2020-12-10 16:39:59 -0800

    Added animalMage tests

--- CrashedAndDeserted/CrashedAndDeserted/unit_test.cpp
+++ CrashedAndDeserted/CrashedAndDeserted/unit_test.cpp
@@ -4,6 +4,9 @@
 #include "Boss.hpp"
 #include "ExiledMember.hpp"
 #include "Dice6AppTest.hpp"
+#include "animalRace.hpp"
+#include "humanRace.hpp"
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
